a51199f2dbdad860eb2532fff29fc60288e84280
sebastientromp <sebastien.tromp@gmail.com>
2023-05-01 11:15:43 +0200

    add fs token generation and return additional details

--- src/auth.ts
+++ src/auth.ts
@@ -5,7 +5,7 @@
 import { getConnection } from '@firestone-hs/aws-lambda-utils';
 import { SecretsManager } from 'aws-sdk';
 import { GetSecretValueRequest, GetSecretValueResponse } from 'aws-sdk/clients/secretsmanager';
-import { JwtPayload, decode } from 'jsonwebtoken';
+import { JwtPayload, decode, sign } from 'jsonwebtoken';
 import fetch from 'node-fetch';
 import SqlString from 'sqlstring';
 import { URLSearchParams } from 'url';
@@ -34,16 +34,16 @@ export default async (event): Promise<any> => {
 
 	const body = JSON.parse(event.body);
 	const code = body.authCode;
-	console.log('processing event 2', code, body);
+	// console.log('processing event 2', code, body);
 
 	const secretRequest: GetSecretValueRequest = {
 		SecretId: 'sso',
 	};
 	const secret: SecretInfo = await getSecret(secretRequest);
-	console.log('got secret', secret);
+	// console.log('got secret', secret);
 
 	const redirect = body.dev ? 'http://localhost:4200' : 'https://www.firestoneapp.gg';
-	console.log('redirect url', redirect);
+	// console.log('redirect url', redirect);
 
 	const params = new URLSearchParams();
 	params.append('client_id', secret.clientId);
@@ -59,7 +59,7 @@ export default async (event): Promise<any> => {
 		},
 	});
 	const authResult = await authResponse.json();
-	console.debug('authResult', authResult);
+	// console.debug('authResult', authResult);
 
 	if (authResult.error) {
 		const stringResults = JSON.stringify({
@@ -75,24 +75,39 @@ export default async (event): Promise<any> => {
 		return response;
 	}
 
-	const decodedJwt: JwtPayload = decode(authResult.access_token) as JwtPayload;
-	console.log('decodedJwt', decodedJwt);
+	const decodedIdToken: JwtPayload & {
+		nickname: string;
+		picture: string;
+		preferred_username: string;
+	} = decode(authResult.id_token) as any;
+	// console.log('decodedIdToken', decodedIdToken);
 
 	const mysql = await getConnection();
 	const queryResult = await mysql.query(`
 		SELECT isPremium FROM user_mapping 
-		WHERE userName = ${SqlString.escape(decodedJwt.sub)}
+		WHERE userName = ${SqlString.escape(decodedIdToken.sub)}
 		LIMIT 1
 	`);
-	const isPremium = decodedJwt.sub === 'daedin' ? true : queryResult[0] ? queryResult[0].isPremium === 1 : false;
-	console.debug('isPremium', isPremium, queryResult);
+	const isPremium = decodedIdToken.sub === 'daedin' ? true : queryResult[0] ? queryResult[0].isPremium === 1 : false;
+	// console.debug('isPremium', isPremium, queryResult);
+
+	// Generate a jxt token that can be used for subsequent calls
+	const userDetails: UserDetails = {
+		userName: decodedIdToken.sub,
+		preferredUsername: decodedIdToken.preferred_username,
+		nickname: decodedIdToken.nickname,
+		picture: decodedIdToken.picture,
+	};
+	const fsToken = generateJwtToken(userDetails, secret.fsJwtTokenKey);
+	const decodedFsTken: JwtPayload = decode(fsToken) as JwtPayload;
 
 	const result: AuthInfo = {
-		userName: decodedJwt.sub,
-		issuedAt: decodedJwt.iat,
-		expiration: decodedJwt.exp,
+		...userDetails,
+		issuedAt: decodedFsTken.iat,
+		expiration: decodedFsTken.exp,
 		premium: isPremium,
 		valid: true,
+		fsToken: fsToken,
 	};
 	const stringResults = JSON.stringify(result);
 	const response = {
@@ -101,6 +116,21 @@ export default async (event): Promise<any> => {
 		headers: headers,
 	};
 	return response;
+};
+
+const generateJwtToken = (userDetails: UserDetails, secret: string): string => {
+	// Generate a JWT token
+	return sign(
+		{
+			...userDetails,
+			sub: userDetails.userName,
+		},
+		secret,
+		{
+			expiresIn: '5d',
+			algorithm: 'HS256',
+		},
+	);
 };
 
 const getSecret = (secretRequest: GetSecretValueRequest) => {
@@ -115,4 +145,12 @@ const getSecret = (secretRequest: GetSecretValueRequest) => {
 interface SecretInfo {
 	readonly clientId: string;
 	readonly clientSecret: string;
+	readonly fsJwtTokenKey: string;
+}
+
+interface UserDetails {
+	readonly userName: string;
+	readonly preferredUsername: string;
+	readonly nickname: string;
+	readonly picture: string;
 }
--- src/model.ts
+++ src/model.ts
@@ -1,7 +1,11 @@
 export interface AuthInfo {
-	readonly valid: boolean;
 	readonly premium: boolean;
 	readonly userName: string;
+	readonly preferredUsername: string;
+	readonly nickname: string;
+	readonly picture: string;
+	readonly valid: boolean;
 	readonly issuedAt: number;
 	readonly expiration: number;
+	readonly fsToken: string;
 }
